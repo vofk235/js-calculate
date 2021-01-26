fffc3a98136c2c55f920d43b5e730a992acedb13
vofk235 <67094664+vofk235@users.noreply.github.com>
2021-01-26 22:34:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const l = document.getElementById("length");
 const w = document.getElementById("width");
 const c = document.getElementById("calc");
 let r = document.getElementById("result");
-let arr = [];
+let arr = g = [];
+
 
 c.addEventListener("click", (evt) => {
+    g[2] = 9;
     prnt(calculate(l.value, w.value));
     evt.preventDefault();
 });
@@ -15,23 +17,64 @@ function calculate(len, wdt) {
     if ((s % 8) == 0) {
         arr[0] = s / 8;
         arr[1] = 0;
+        arr[2] = 0;
         return arr;
-    } else if (((s % 8) % 6) == 0) {
-        arr[0] = Math.floor(s / 8);
-        arr[1] = 1;
+    } else if (s > 14 && SearchBox(s)) {
         return arr;
     } else if ((s % 6) == 0) {
         arr[0] = 0;
         arr[1] = s / 6;
+        arr[2] = 0;
         return arr;
     } else {
-        if ((s % 8) < 6) {
-            arr[0] = Math.floor(s / 8);
-            arr[1] = 1;
-            return arr;
-        } else {
-            arr[0] = Math.ceil(s / 8);
-            arr[1] = 0;
+        ForArray(s);
+        arr[0] = g[0];
+        arr[1] = g[1];
+        arr[2] = g[2];
+        return arr;
+    }
+}
+
+function returnBalance(q, e, c) {
+    let r = q - ((e * 8) + (c * 6));
+    if (r > 0) {
+        return false;
+    }
+    r = Math.abs(r);
+    if (3 <= r <= 7) {
+        let mass = [];
+        mass[0] = e;
+        mass[1] = c;
+        mass[2] = r;
+        return mass;
+    }
+}
+
+function ForArray(w) {
+    let f = [];
+    for (let m = 0; m <= 29; m++) {
+        for (let n = 0; n <= 38; n++) {
+            f = returnBalance(w, n, m);
+            if (f != "false") {
+                if (g[2] != 0 && g[2] != undefined) {
+                    if (f[2] < g[2]) {
+                        g[0] = f[0];
+                        g[1] = f[1];
+                        g[2] = f[2];
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+function SearchBox(b) {
+    for (let i = 0; i < 4; i++) {
+        if (((b - (i * 6)) % 8) == 0 && ((6 * i) % 6) == 0) {
+            arr[0] = (b - (i * 6)) / 8;
+            arr[1] = i;
+            arr[2] = 0;
             return arr;
         }
     }
@@ -47,6 +90,9 @@ function prnt(array) {
     }
     if (array[1]) {
         t += `${array[1]} packs of 6pcs`;
+    }
+    if (array[2]) {
+        t += `, remaining free seats - ${array[2]}`;
     }
     r.innerHTML = t;
 }
